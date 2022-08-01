e512b1216f2195d347fed4fd8a8b863ad21c9681
Yury Zhuk <yury.zhuk@ewave.com>
2022-08-01 13:25:01 +0300

    init CRA app

---
+++ src/data/product.js
@@ -0,0 +1,10 @@
+import img from '../static/images/P00704075.webp';
+
+export const product = {
+    name: 'EMILIA WICKSTEAD',
+    sku: 'P00704075',
+    img: img,
+    description: 'Mara floral faille midi dress',
+    price: '1,295',
+    sizes: [34, 36, 38]
+}
---
+++ src/data/products.js
@@ -0,0 +1,91 @@
+import P00704075 from '../static/images/P00704075.webp';
+import P00701158 from "../static/images/P00701158.webp";
+import P00705201 from "../static/images/P00705201.webp";
+import P00695839 from "../static/images/P00695839.webp";
+import P00667934 from "../static/images/P00667934.webp";
+import P00693032 from "../static/images/P00693032.webp";
+import P00713770 from "../static/images/P00713770.webp";
+import P00717616 from "../static/images/P00717616.webp";
+import P00694104 from "../static/images/P00694104.webp";
+
+export const products = [
+    {
+        name: 'Emilia wickstead',
+        sku: 'P00704075',
+        img: P00704075,
+        description: 'Mara floral faille midi dress',
+        price: '1,295',
+        sizes: [34, 36, 38],
+        type: 'clothes'
+    },
+    {
+        name: 'Balmain',
+        sku: 'P00701158',
+        img: P00701158,
+        description: 'Square-neck wool minidress',
+        price: '1,455',
+        sizes: [28, 30],
+        type: 'clothes'
+    },
+    {
+        name: 'Dolce&Gabbana',
+        sku: 'P00705201',
+        img: P00705201,
+        description: 'Printed cotton poplin minidress',
+        price: '839',
+        sizes: [28, 30, 32, 34],
+        type: 'clothes'
+    },
+    {
+        name: 'Dolce&Gabbana',
+        sku: 'P00695839',
+        img: P00695839,
+        description: 'Ruffled faille midi dress',
+        price: '2509',
+        sizes: [32, 34],
+        type: 'clothes'
+    },
+    {
+        name: 'Christian Louboutin',
+        sku: 'P00667934',
+        img: P00667934,
+        description: 'Cabarock Small leather tote bag',
+        price: '1085',
+        type: 'bag'
+    },
+    {
+        name: 'Givenchy',
+        sku: 'P00693032',
+        img: P00693032,
+        description: 'Moon Cut Out Small canvas shoulder bag',
+        price: '820',
+        type: 'bag'
+    },
+    {
+        name: 'Malone Souliers',
+        sku: 'P00713770',
+        img: P00713770,
+        description: 'Satin platform sandals',
+        price: '529',
+        sizes: [35, 36, 37, 38],
+        type: 'shoes'
+    },
+    {
+        name: 'Souliers Martinez',
+        sku: 'P00717616',
+        img: P00717616,
+        description: 'Springs leather platform sandals',
+        price: '329',
+        sizes: [36, 38, 39],
+        type: 'shoes'
+    },
+    {
+        name: 'Valentino Garavani',
+        sku: 'P00694104',
+        img: P00694104,
+        description: 'Rockstud embellished leather pumps',
+        price: '1465',
+        sizes: [38],
+        type: 'shoes'
+    },
+]
